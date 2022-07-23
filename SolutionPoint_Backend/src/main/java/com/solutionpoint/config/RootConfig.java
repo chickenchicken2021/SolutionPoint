0c3adb01cd3216bff0c3bc3310b5948ff743e747
JSConF <arare30030@naver.com>
2022-07-23 19:09:39 +0900

    XML에서 Java Config로 수정

---
+++ SolutionPoint_Backend/src/main/java/com/solutionpoint/config/RootConfig.java
@@ -0,0 +1,36 @@
+package com.solutionpoint.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+@ComponentScan(basePackages = {"com.solutionpoint.mapper"})
+public class RootConfig {
+
+    @Bean
+    public DataSource dataSource(){
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setDriverClassName("com.mysql.jdbc.Driver");
+        hikariConfig.setJdbcUrl("jdbc:mysql://localhost:3307/com?serverTimezone=UTC");
+        hikariConfig.setUsername("solution");
+        hikariConfig.setPassword("solution1234");
+
+        HikariDataSource dataSource = new HikariDataSource(hikariConfig);
+        return dataSource;
+    }
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory() throws Exception{
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(dataSource());
+        return sqlSessionFactoryBean.getObject();
+    }
+
+}
